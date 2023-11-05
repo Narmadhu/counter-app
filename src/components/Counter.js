2c7c52977d59251c24c9fa909dc7800ffda5d0ca
Narmadhu <oknarmadhu10496@gmail.com>
2023-11-05 18:48:47 +0530

    added style using  css and material ui

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,6 @@
+import { Button } from "@mui/material";
 import React, { useState } from "react";
+import "../App.css";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
@@ -14,11 +16,38 @@ export default function Counter() {
     setCount(0);
   };
   return (
-    <>
-      <h1>Current Count: {count}</h1>
-      <button onClick={decreaseCount}>Decrease</button>
-      <button onClick={increaseCount}>Increase</button>
-      <button onClick={resetCount}>Reset</button>
-    </>
+    <div className="content">
+      <h1 style={{ color: "white" }}>Current Count: {count}</h1>
+      <div className="btns">
+        <Button
+          className="btn"
+          variant="outlined"
+          onClick={decreaseCount}
+          style={{ color: "white" }}
+        >
+          Decrease
+        </Button>
+        <Button
+          className="btn"
+          variant="outlined"
+          onClick={increaseCount}
+          style={{ color: "white" }}
+        >
+          Increase
+        </Button>
+        <Button
+          className="btn"
+          variant="outlined"
+          onClick={resetCount}
+          style={{ color: "white" }}
+        >
+          Reset
+        </Button>
+      </div>
+
+      {/* <button onClick={decreaseCount}>Decrease</button> */}
+      {/* <button onClick={increaseCount}>Increase</button> */}
+      {/* <button onClick={resetCount}>Reset</button> */}
+    </div>
   );
 }
